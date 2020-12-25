c76c92a549f032232e9dc1f5c98586b1f20370f0
Huy <69767329+Huy-jpg@users.noreply.github.com>
2020-12-25 13:44:26 -0800

    commit on 'master'

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const containerQs = document.querySelectorAll('.detail-questions'),
-      answers = document.querySelectorAll('.answers');
+const containerQs = document.querySelectorAll('.detail-questions');
 
 containerQs.forEach(container => {
-  container.addEventListener('click', () => {
-    answers.forEach(ans => {
-      if(ans.classList.contains('active')){
-        ans.classList.remove('active');
-      } else{
-        ans.classList.add('active');
-      }
-    })
+  container.addEventListener('click', () =>{
+    container.nextElementSibling.classList.toggle('active');
   })
 })

52f16d3cb1aa09e11ee4bc0590049b6fcf45d285
Huy <69767329+Huy-jpg@users.noreply.github.com>
2020-12-25 14:40:01 -0800

    commit on 'master'

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const containerQs = document.querySelectorAll('.detail-questions');
+const containerQs = document.querySelectorAll('.detail-questions')
+      arrows = document.querySelectorAll('.arrow');
 
 containerQs.forEach(container => {
   container.addEventListener('click', () =>{
     container.nextElementSibling.classList.toggle('active');
+    container.nextElementSibling.nextElementSibling.classList.toggle('transition');
   })
 })
